071f9e8110cf693af427405d167068bb7af6ee9d
Rishabh <rishabhsahu106@gmail.com>
2021-06-09 18:20:37 +0530

    Init commit

---
+++ aosp_RMX2185.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2020 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile
+$(call inherit-product, device/realme/RMX2185/device.mk)
+
+# Inherit some AOSP OS stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/aosp/configs/common.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_RMX2185
+PRODUCT_DEVICE := RMX2185
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme G35 Series
+PRODUCT_MANUFACTURER := realme
+
+# Build info
+BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ2A.210405.005/7181113:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=RMX2185 \
+    PRODUCT_NAME=RMX2185 \
+    PRIVATE_BUILD_DESC="full_oppo6765-user 10 QP1A.190711.020 68b77aba7cb33275 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
